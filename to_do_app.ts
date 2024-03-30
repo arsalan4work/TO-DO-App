238be407fa904d2d3833a3a21f7663a79c0fa448
arsalan4work <you@example.com>
2024-03-30 17:58:39 +0500

    first commit

---
+++ to_do_app.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+// This is TO-DO App
+
+import inquirer from "inquirer"
+
+let todo = [];
+let condition = true
+
+// Using While Loop
+while(condition)
+{
+     let addtask = await inquirer.prompt(
+        [
+            {
+            name:"todo",
+            type:"input",
+            message:"What you want to add in your list?"
+            },
+              
+            {
+            name:"addmore",
+            type:"confirm",
+            message:"Do you want to add more?",
+            default: "false"
+            }
+            
+        ]
+    );
+ todo.push (addtask.todo);
+ condition = addtask.addmore
+ console.log(todo)
+
+}
